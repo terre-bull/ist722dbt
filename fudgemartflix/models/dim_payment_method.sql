6565c60633db852f52a8e7ba782c27f791598156
Diana Simonson <dsimonso@syr.edu>
2026-03-09 01:04:36 +0000

    Removing card number and expiration

--- fudgemartflix/models/dim_payment_method.sql
+++ fudgemartflix/models/dim_payment_method.sql
@@ -3,8 +3,6 @@ with fudgeflix_payment as (
     select
         0                                       as payment_id,
         'Not Applicable'                        as payment_type,
-        null::varchar                           as payment_exp_date,
-        null::varchar                           as payment_credit_card,
         'FudgeFlix'                             as division
 ),
 
@@ -12,8 +10,6 @@ fudgemart_payment as (
     select
         creditcard_id                           as payment_id,
         'Credit Card'                           as payment_type,
-        creditcard_exp_date                     as payment_exp_date,
-        creditcard_number                       as payment_credit_card,
         'FudgeMart'                             as division
     from {{ source('fudgemart_v3', 'fm_creditcards') }}
 ),
