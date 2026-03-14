0424817d9d80fd5a522dfec48530821db5246b7e
Diana Simonson <dsimonso@syr.edu>
2026-03-14 18:39:46 +0000

    Renaming fact_sale columns for clarity

--- fudgemartflix/models/fact_sale.sql
+++ fudgemartflix/models/fact_sale.sql
@@ -2,15 +2,15 @@ with fudgeflix_sales as (
     select
         ab.ab_id                                                    as order_id,
         dc.customer_key                                             as customer_key,
-        replace(to_date(ab.ab_date)::varchar, '-', '')::int         as date_key,
+        replace(to_date(ab.ab_date)::varchar, '-', '')::int         as order_date_key,
         dp.product_key                                              as product_key,
         dpm.payment_method_key                                      as payment_method_key,
         2                                                           as channel_key,
         1                                                           as order_quantity,
         ab.ab_billed_amount                                         as unit_selling_price,
         ab.ab_billed_amount / 2                                     as unit_cost_price,
-        ab.ab_billed_amount                                         as sold_amount,
-        ab.ab_billed_amount / 2                                     as cost_amount,
+        ab.ab_billed_amount                                         as order_sold_amount,
+        ab.ab_billed_amount / 2                                     as order_cost_amount,
         ab.ab_billed_amount - (ab.ab_billed_amount / 2)             as order_profit,
         CASE WHEN ab.ab_billed_amount = 0 OR ab.ab_billed_amount IS NULL
              THEN NULL
@@ -33,15 +33,15 @@ fudgemart_sales as (
     select
         o.order_id                                                  as order_id,
         dc.customer_key                                             as customer_key,
-        replace(to_date(o.order_date)::varchar, '-', '')::int       as date_key,
+        replace(to_date(o.order_date)::varchar, '-', '')::int       as order_date_key,
         dp.product_key                                              as product_key,
         dpm.payment_method_key                                      as payment_method_key,
         1                                                           as channel_key,
         od.order_qty                                                as order_quantity,
         p.product_retail_price                                      as unit_selling_price,
         p.product_wholesale_price                                   as unit_cost_price,
-        od.order_qty * p.product_retail_price                       as sold_amount,
-        od.order_qty * p.product_wholesale_price                    as cost_amount,
+        od.order_qty * p.product_retail_price                       as order_sold_amount,
+        od.order_qty * p.product_wholesale_price                    as order_cost_amount,
         (od.order_qty * p.product_retail_price)
             - (od.order_qty * p.product_wholesale_price)            as order_profit,
         CASE WHEN (od.order_qty * p.product_retail_price) = 0 OR (od.order_qty * p.product_retail_price) IS NULL
