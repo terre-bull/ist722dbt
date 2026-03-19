3f8ef02315e3d9b2505c441d3bac1d6c3b9bd51a
Diana Simonson <dsimonso@syr.edu>
2026-03-19 23:36:28 +0000

    Removing dim_sale_channel

--- fudgemartflix/models/dim_sale_channel.sql
+++
@@ -1,7 +0,0 @@
-with channels as (
-    select 1 as channel_key, 'FudgeMart' as channel_name, 'Retail'       as channel_model
-    union all
-    select 2 as channel_key, 'FudgeFlix' as channel_name, 'Subscription' as channel_model
-)
-
-select * from channels
